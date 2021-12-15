c0ded68fe024834a8c14e073680329a310349686
FandiWR <fandi.wahyur99@gmail.com>
2021-12-15 18:50:29 +0700

    solve bug mental kalo nembak & add spawn point bear

--- VhunteR/Assets/Scripts/EnemyManager.cs
+++ VhunteR/Assets/Scripts/EnemyManager.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
     void Start()
     {
-        
+        //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Spawn()
     {
-        
+        //Jika player telah mati maka tidak membuat enemy baru
+
+        //Mendapatkan nilai random
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        //Memduplikasi enemy
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
--- VhunteR/Assets/Scripts/Gun.cs
+++ VhunteR/Assets/Scripts/Gun.cs
@@ -22,6 +22,8 @@ public class Gun : MonoBehaviour
         }
     }
 
+    
+
     public void Shoot()
     {
         if (muzzleFlashPrefab)
